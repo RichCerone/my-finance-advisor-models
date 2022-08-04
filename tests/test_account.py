baf6c3fda27f26cb38fbc4aa037dc739a7513e20
Rich Cerone <rcerone@live.com>
2022-08-04 08:05:09 -0400

    Added account owner field to Account model.

--- tests/test_account.py
+++ tests/test_account.py
@@ -13,13 +13,14 @@ class AccountTests(unittest.TestCase):
         self.assertEqual("account::some_id", account.id)
 
         # Full constructor test.
-        account = Account("some_id", "some_name", "some_type", "some_institution", Decimal("100.00"))
+        account = Account("some_id", "some_name", "some_type", "some_institution", "some_owner", Decimal("100.00"))
 
         self.assertEqual("account::some_id", account.id)
         self.assertEqual("some_id", account.account_id)
         self.assertEqual("some_name", account.account_name)
         self.assertEqual("some_type", account.account_type)
         self.assertEqual("some_institution", account.account_institution)
+        self.assertEqual("some_owner", account.account_owner_id)
         self.assertEqual(100.00, account.balance)
 
     # Asserts validation works on the account parameters.
@@ -40,13 +41,17 @@ class AccountTests(unittest.TestCase):
         with self.assertRaises(ValueError):
             Account("some_id", "some_name", "some_type", None)
 
+        # Bad account owner id.
+        with self.assertRaises(ValueError):
+            Account("some_id", "some_name", "some_type", "some_institution", None)
+
         # Bad account balance.
         with self.assertRaises(TypeError):
-            Account("some_id", "some_name", "some_type", "some_institution", None)
+            Account("some_id", "some_name", "some_type", "some_institution", "some_owner", None)
 
     # Asserts the to string format is correct.
     def test_account_to_str(self):
-        account = Account("some_id", "some_name", "some_type", "some_institution", Decimal("100.00"))
-        expected_str = "'id': 'account::some_id' | 'account_id': 'some_id' | 'account_name': 'some_name' | 'account_type': 'some_type' | 'account_institution': 'some_institution' | 'balance': '$100.00'"
+        account = Account("some_id", "some_name", "some_type", "some_institution", "some_owner", Decimal("100.00"))
+        expected_str = "'id': 'account::some_id' | 'account_id': 'some_id' | 'account_name': 'some_name' | 'account_type': 'some_type' | 'account_institution': 'some_institution' | 'account_owner_id': 'some_owner' | 'balance': '$100.00'"
 
         self.assertEqual(expected_str, account.__str__())
